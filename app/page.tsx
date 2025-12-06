7216ef10562e1bbf154f7942d337fa29fd73bfac
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2025-12-07 02:41:20 +0545

    asdfasdf

--- app/page.tsx
+++ app/page.tsx
@@ -16,57 +16,69 @@ export default function Home() {
   return (
     <div>
 
-      <div className="lg:px-6   px-4">
-        <div className="flex lg:justify-start justify-center w-full my-4 text-[#2460E9]">
-          <span className="py-2 px-4 bg-[#DDEBFE] rounded-3xl text-sm flex items-center gap-2">
+      <div className="lg:px-6   px-4 mt-6 lg:mt-12">
 
-            <ReactCountryFlag countryCode="IN" svg style={{ width: "1.5em", height: "1.5em" }} />
 
-            <span>→</span>
+        <div className="flex items-center justify-between">
 
-            <ReactCountryFlag countryCode="NP" svg style={{ width: "1.5em", height: "1.5em" }} />
 
-            Order from any Indian site, pay in NPR
-          </span>
+          <div>
+            <div className="flex lg:justify-start justify-center w-full my-4 text-[#2460E9]">
+              <span className="py-2 px-4 bg-[#DDEBFE] rounded-3xl text-sm flex items-center gap-2">
 
+                <ReactCountryFlag countryCode="IN" svg style={{ width: "1.5em", height: "1.5em" }} />
+
+                <span>→</span>
+
+                <ReactCountryFlag countryCode="NP" svg style={{ width: "1.5em", height: "1.5em" }} />
+
+                Order from any Indian site, pay in NPR
+              </span>
+
+
+            </div>
+
+            <TopLayerLandingPage />
+
+
+            <div className="lg:w-3/5">
+
+              <span className="text-themeBlue font-medium ">Paste the product link from Amazon, Flipkart, Myntra or any Indian store. We handle buying, customs and shipping — you pay once in NPR and relax.</span>
+            </div>
+
+
+            {/* Buttons */}
+            <div className="flex items-center justify-between gap-2 my-8 max-w-[600px] sm:gap-12">
+
+              <Link
+                href="/order"
+                className="bg-themeBlue text-white flex-1 py-3 rounded-[26px] text-sm cursor-pointer inline-flex justify-center items-center"
+                style={{
+                  boxShadow: '0 4px 6px rgba(1, 49, 89, 0.5)' // themeBlue rgba with 50% opacity
+                }}
+              >
+                Create your first order
+              </Link>
+
+
+              <Link
+                href="/price-calculator"
+                className="bg-[#F8862A] text-white flex-1 py-3 rounded-[26px] text-sm cursor-pointer inline-flex justify-center items-center"
+                style={{
+                  boxShadow: '0 4px 6px rgba(248, 134, 42, 0.5)',
+                }}
+              >
+                Try cost calculator
+              </Link>
+
+
+            </div>
+          </div>
+
+          <img className=" max-w-[500px] xl:max-w-[600px] 2xl:max-w-[700px] lg:flex hidden" src="/landingPage/box4.png" alt="" />
 
         </div>
 
-        <TopLayerLandingPage />
-
-
-        <div className="lg:w-3/5">
-
-          <span className="text-themeBlue font-medium ">Paste the product link from Amazon, Flipkart, Myntra or any Indian store. We handle buying, customs and shipping — you pay once in NPR and relax.</span>
-        </div>
-
-
-        {/* Buttons */}
-        <div className="flex items-center justify-between gap-2 my-8 max-w-[600px] sm:gap-12">
-
-          <Link
-            href="/order"
-            className="bg-themeBlue text-white flex-1 py-3 rounded-[26px] text-sm cursor-pointer inline-flex justify-center items-center"
-            style={{
-              boxShadow: '0 4px 6px rgba(1, 49, 89, 0.5)' // themeBlue rgba with 50% opacity
-            }}
-          >
-            Create your first order
-          </Link>
-
-
-          <Link
-            href="/price-calculator"
-            className="bg-[#F8862A] text-white flex-1 py-3 rounded-[26px] text-sm cursor-pointer inline-flex justify-center items-center"
-            style={{
-              boxShadow: '0 4px 6px rgba(248, 134, 42, 0.5)',
-            }}
-          >
-            Try cost calculator
-          </Link>
-
-
-        </div>
 
         {/* Customer Stats  */}
         <UserRating />
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,82 +3,151 @@
 import { Fragment } from "react";
 import { Disclosure, Transition } from "@headlessui/react";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { usePathname } from "next/navigation";
 import { Link } from "next-view-transitions";
+import React, { useState } from "react";
+
+import {
+  easeInOut,
+  motion,
+  useMotionTemplate,
+  useMotionValueEvent,
+  useScroll,
+  useTransform,
+} from "motion/react";
 
 const navigation = [
-  { name: "Home", href: "/", current: true },
-  { name: "Price-Calculator", href: "/price-calculator", current: false },
-  { name: "FAQs", href: "/faqs", current: false },
-  { name: "WhatsApp Support", href: "https://wa.me/9779817254118", current: false },
+  { name: "Home", href: "/" },
+  { name: "Price-Calculator", href: "/price-calculator" },
+  { name: "FAQs", href: "/faqs" },
+  { name: "WhatsApp Support", href: "https://wa.me/9779817254118" },
 ];
 
 const Navbar = () => {
+
+  const { scrollY } = useScroll();
+
+  const [scrolled, setScrolled] = useState<boolean>(false);
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    if (latest > 10) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  });
+
+  const pathname = usePathname();
+
   return (
-    <Disclosure as="nav" className="bg-white">
-      {({ open }) => (
-        <>
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="flex justify-between h-16 items-center">
 
-              {/* Logo */}
-              <div className="flex-shrink-0">
-                <Link href="/">
-                  <img src="/logo.png" alt="Logo" className="w-[180px]" />
-                </Link>
-              </div>
+    <motion.div
+      animate={{
+        boxShadow: scrolled ? "var(--shadow-acertinity)" : "none",
+        width: scrolled ? "90%" : "100%",
+        borderRadius: scrolled ? "26px" : "0px", // 8px = 0.5rem
+        y: scrolled ? 10 : 0,
 
-              {/* Desktop nav links */}
-              <div className="hidden md:flex space-x-4">
-                {navigation.map((item) => (
-                  <Link
-                    key={item.name}
-                    href={item.href}
-                    className={`px-3 py-2 rounded-md text-sm font-medium ${item.current ? "text-white bg-themeBlue" : "text-primary hover:bg-gray-200"
-                      }`}
-                  >
-                    {item.name}
+      }}
+      transition={{
+        duration: 0.3,
+        ease: easeInOut,
+      }}
+      className="fixed z-50 inset-x-0 top-0 mx-auto bg-white"
+    >
+
+      <Disclosure as="nav" className="">
+        {({ open }) => (
+          <>
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+              <div className="flex justify-between h-16 items-center">
+
+                {/* Logo */}
+                <div className="flex-shrink-0">
+                  <Link href="/">
+                    <img src="/logo.png" alt="Logo" className="w-[180px]" />
                   </Link>
-                ))}
-              </div>
-              
+                </div>
 
-              {/* Mobile menu button */}
-              <div className="md:hidden">
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-700 hover:text-white hover:bg-themeBlue focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  {open ? <FaTimes size={24} /> : <FaBars size={24} />}
-                </Disclosure.Button>
+                {/* Desktop nav links */}
+                <div className="hidden lg:flex space-x-4">
+                  {navigation.map((item) => {
+                    const isCurrent = item.href === pathname && !item.href.startsWith("https://wa");
+                    return (
+                      <Link
+                        key={item.name}
+                        href={item.href}
+                        className={`px-3 py-2 rounded-md text-sm font-medium ${isCurrent ? "text-white bg-themeBlue" : "text-primary hover:bg-gray-200"
+                          }`}
+                      >
+                        {item.name}
+                      </Link>
+                    );
+                  })}
+                </div>
+
+                {/* Action buttons */}
+                <div className="gap-6 items-center hidden lg:flex">
+                  <Link
+                    href="/login"
+                    className="px-3 py-2 rounded-md text-sm font-medium hover:underline"
+                  >
+                    Login
+                  </Link>
+                  <Link
+                    href="/order"
+                    className="bg-themeBlue text-white flex-1 py-3 rounded-[26px] text-sm cursor-pointer inline-flex justify-center items-center px-6"
+                    style={{
+                      boxShadow: '0 4px 6px rgba(1, 49, 89, 0.5)'
+                    }}
+                  >
+                    Create your first order
+                  </Link>
+                </div>
+
+                {/* Mobile menu button */}
+                <div className="lg:hidden">
+                  <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-700 hover:text-white hover:bg-themeBlue focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                    {open ? <FaTimes size={24} /> : <FaBars size={24} />}
+                  </Disclosure.Button>
+                </div>
               </div>
             </div>
-          </div>
 
-          {/* Mobile menu with animation */}
-          <Transition
-            as={Fragment}
-            enter="transition duration-200 ease-out"
-            enterFrom="transform opacity-0 -translate-y-2"
-            enterTo="transform opacity-100 translate-y-0"
-            leave="transition duration-150 ease-in"
-            leaveFrom="transform opacity-100 translate-y-0"
-            leaveTo="transform opacity-0 -translate-y-2"
-          >
-            <Disclosure.Panel className="md:hidden">
-              <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                {navigation.map((item) => (
-                  <Link
-                    key={item.name}
-                    href={item.href}
-                    className={`block px-3 py-2 rounded-md text-base  ${item.current ? "text-white bg-themeBlue" : "text-secondary hover:bg-gray-200"
-                      }`}
-                  >
-                    {item.name}
-                  </Link>
-                ))}
-              </div>
-            </Disclosure.Panel>
-          </Transition>
-        </>
-      )}
-    </Disclosure>
+            {/* Mobile menu */}
+            <Transition
+              as={Fragment}
+              enter="transition duration-200 ease-out"
+              enterFrom="transform opacity-0 -translate-y-2"
+              enterTo="transform opacity-100 translate-y-0"
+              leave="transition duration-150 ease-in"
+              leaveFrom="transform opacity-100 translate-y-0"
+              leaveTo="transform opacity-0 -translate-y-2"
+            >
+              <Disclosure.Panel className="lg:hidden">
+                <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                  {navigation.map((item) => {
+                    const isCurrent = item.href === pathname && !item.href.startsWith("https://wa");
+                    return (
+                      <Link
+                        key={item.name}
+                        href={item.href}
+                        className={`block px-3 py-2 rounded-md text-base ${isCurrent ? "text-white bg-themeBlue" : "text-secondary hover:bg-gray-200"
+                          }`}
+                      >
+                        {item.name}
+                      </Link>
+                    );
+                  })}
+                </div>
+              </Disclosure.Panel>
+            </Transition>
+          </>
+        )}
+      </Disclosure>
+
+    </motion.div>
+
   );
 };
 
