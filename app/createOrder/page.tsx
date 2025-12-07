76c595327cc81a461cb037c78122acf6e69c236d
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2025-12-08 02:19:00 +0545

    sdafasdf

--- app/createOrder/page.tsx
+++ app/createOrder/page.tsx
@@ -188,7 +188,7 @@ const CreateOrder: React.FC = () => {
                     rows={3}
                     value={address}
                     onChange={(e) => setAddress(e.target.value)}
-                    placeholder="Customer Delevery Address"
+                    placeholder="Customer Delivery Address"
                     className="w-full border border-gray-300 rounded-lg px-3 py-2 mb-3 text-sm resize-none focus:ring-2 focus:ring-blue-500 outline-none"
                 />
 
